e55a5a18cf7c95f2ec147aa785e7060567b5fb25
Faymek Feng <faymek.f@gmail.com>
2020-10-24 21:32:22 +0800

    clean code

--- cbatch.py
+++ cbatch.py
@@ -1,6 +1,10 @@
 #%%
-import os, sys, time
-import re, glob, json
+import os
+import sys
+import time
+import re
+import glob
+import json
 import argparse
 import subprocess
 from multiprocessing import Pool
@@ -25,15 +29,12 @@ HOST = {
 
 TRANSLATE = {}
 LOG_KEYS = {
-    "psnr": ['y', 'u', 'v', 'avg', 'min', 'max'],
-    "ssim": ['y', 'u', 'v', 'all'],
-    "vmaf": ['vmaf'],
     "VTM": ['Frames', '|', 'Bitrate', 'Y-PSNR', 'U-PSNR', 'V-PSNR', 'YUV-PSNR', 'Time'],
     "HM": ['Frames', '|', 'Bitrate', 'Y-PSNR', 'U-PSNR', 'V-PSNR', 'YUV-PSNR', 'Time'],
     "HPM": ['Y-PSNR', 'U-PSNR', 'V-PSNR', 'Y-MSSSIM', 'Bits', 'Bitrate', 'Frames', 'Time']
 }
 
-META =  {
+META = {
     "InputBitDepth": "8",
     "InputChromaFormat": "",
     "FrameRate": "",
@@ -43,13 +44,16 @@ META =  {
     "PixelFormat": ""
 }
 
+
 class BaseConf:
     def __init__(self):
         pass
 
+
 def post(addr, pf, base=None):
     request = Request(Conf.baseurl+addr, urlencode(pf).encode())
     return urlopen(request).read().decode()
+
 
 def get(addr):
     request = Request(Conf.baseurl+addr)
@@ -70,6 +74,7 @@ def log_vtm(fn):
         else:
             return "excute", nline-10, None
 
+
 def log_hm(fn):
     with open(fn, "r") as f:
         lines = list(f.readlines())
@@ -82,6 +87,7 @@ def log_hm(fn):
             return "finish", nline-92, values
         else:
             return "excute", nline-68, None
+
 
 def log_hpm(fn):
     with open(fn, "r") as f:
@@ -99,12 +105,13 @@ def log_hpm(fn):
         else:
             return "excute", nline-48, None
 
+
 def log_getEnctype(fn):
     enctype = ""
     with open(fn, "r") as f:
         lines = list(f.readlines())
         nline = len(lines)
-        if nline>1:
+        if nline > 1:
             if lines[1].startswith("VVCSoftware: VTM Encoder Version"):
                 enctype = "VTM"
             elif lines[1].startswith("HM software: Encoder Version"):
@@ -113,8 +120,9 @@ def log_getEnctype(fn):
                 enctype = "HPM"
     return enctype
 
+
 def log_adapter(fn, enctype=""):
-    if not enctype: # interpret
+    if not enctype:  # interpret
         enctype = log_getEnctype(fn)
     dict_func = {
         "VTM": log_vtm,
@@ -132,9 +140,10 @@ def readyuv420(filename, bitdepth, W, H):
     pixelsPerFrame = int(H) * int(W) * 3 // 2
     bytesPerFrame = bytesPerPixel * pixelsPerFrame
     fp = open(filename, 'rb')
-    fp.seek(0,2)
+    fp.seek(0, 2)
     totalframe = fp.tell() // bytesPerFrame
     return str(totalframe)
+
 
 def meta_fn(fn, calcFrames=False):
     meta = META.copy()
@@ -151,23 +160,27 @@ def meta_fn(fn, calcFrames=False):
         elif re.match(r"^[0-9]*$", item):
             meta["FrameRate"] = item
     if calcFrames:
-        meta["AllFrames"] = readyuv420(fn, meta["InputBitDepth"], meta["SourceWidth"], meta["SourceHeight"])
+        meta["AllFrames"] = readyuv420(
+            fn, meta["InputBitDepth"], meta["SourceWidth"], meta["SourceHeight"])
     if not meta["InputChromaFormat"]:
         meta["InputChromaFormat"] = Conf.cf
     meta["PixelFormat"] = "yuv{}p".format(meta["InputChromaFormat"])
     if meta["InputBitDepth"] == "10":
         meta["PixelFormat"] = meta["PixelFormat"]+"10le"
     return meta
-    
+
+
 def getabspath(s):
-    return os.path.abspath(os.path.expanduser(s))\
+    return os.path.abspath(os.path.expanduser(s))
+
 
 def yuvopt(fn):
-    opt = "-i %s"%fn
+    opt = "-i %s" % fn
     if fn.endswith(".yuv"):
         yuvinfo = " -s {SourceWidth}x{SourceHeight} -pix_fmt {PixelFormat} "
         opt = yuvinfo.format(**meta_fn(fn)) + opt
     return opt
+
 
 def metric(enc, ref, mode='psnr', onlykey=False):
     shellmap = {
@@ -175,13 +188,14 @@ def metric(enc, ref, mode='psnr', onlykey=False):
         "ssim": "ffmpeg -v info %s %s -filter_complex ssim -f null -y - 2>&1",
         "vmaf": "ffmpeg -v info %s %s -filter_complex libvmaf -f null -y - 2>&1",
     }
-    cmd = shellmap[mode]%(yuvopt(enc), yuvopt(ref))
+    cmd = shellmap[mode] % (yuvopt(enc), yuvopt(ref))
     line = list(os.popen(cmd).readlines())[-1]
-    line = line.replace("score: ","vmaf:")
+    line = line.replace("score: ", "vmaf:")
     items = line[:-1].split(' ')[4:]
     if onlykey:
         return [item.split(':')[0] for item in items if ':' in item]
     data = [item.split(':')[1] for item in items if ':' in item]
+    info = ""
     if "PSNR" in line:
         info = ' '.join(items[:3])
     elif "SSIM" in line:
@@ -190,20 +204,21 @@ def metric(enc, ref, mode='psnr', onlykey=False):
         info = items[-1]
     return data, info
 
+
 def measure(inpath, outpath, mode='psnr'):
     results = []
     inglob = inpath + "/*"
     for fin in sorted(glob.glob(inglob)):
         inname = '_'.join(os.path.basename(fin).split('_')[:-1])
-        outglob = "{}/{}*".format(outpath,inname)
+        outglob = "{}/{}*".format(outpath, inname)
         for fout in sorted(glob.glob(outglob)):
             outname = os.path.basename(fout)
-            data, info = metric(fin,fout, mode)
+            data, info = metric(fin, fout, mode)
             results.append([outname]+data)
-            print("%-48s %s"%(outname, info))
+            print("%-48s %s" % (outname, info))
     with open("measure.csv", "w") as f:
         if results:
-            keys = metric(fin,fout, mode, onlykey=True)
+            keys = metric(fin, fout, mode, onlykey=True)
             f.write('fn,'+','.join(keys)+'\n')
             f.writelines([','.join(item)+'\n' for item in results])
         else:
@@ -215,7 +230,7 @@ def yuv1stframe(inpath, outpath):
     inglob = "{inpath}/*.yuv".format(inpath=inpath)
     for fin in glob.glob(inglob):
         inname = os.path.basename(fin).split('.')[0]
-        fout = "{outpath}/{inname}".format(outpath=outpath,inname=inname)
+        fout = "{outpath}/{inname}".format(outpath=outpath, inname=inname)
         meta = meta_fn(fin)
         cmd = shell.format(fin=fin, **meta, fout=fout)
         TASKS.append(cmd)
@@ -224,16 +239,16 @@ def yuv1stframe(inpath, outpath):
 def convert(inpath, outpath, mode="png2yuv"):
     fmtmap = {
         "png2yuv": "ffmpeg -y -i {fin} -pix_fmt {PixelFormat} {fout}.yuv",
-        "png2rgb": "ffmpeg -y -i {fin} -pix_fmt rgba {fout}.rgb",
+        "png2rgb": "ffmpeg -y -i {fin} -pix_fmt rgb24 {fout}.rgb",
         "yuv2png": "ffmpeg -y -f rawvideo -pixel_format {PixelFormat} -video_size {SourceWidth}x{SourceHeight} -i {fin} -vframes 1 {fout}.png",
-        "rgb2png": "ffmpeg -y -f rawvideo -pixel_format rgba -video_size {SourceWidth}x{SourceHeight} -i {fin} -vframes 1 {fout}.png"
+        "rgb2png": "ffmpeg -y -f rawvideo -pixel_format rgb24 -video_size {SourceWidth}x{SourceHeight} -i {fin} -vframes 1 {fout}.png"
     }
     shell = fmtmap[mode]
     fmt1, fmt2 = mode.split('2')
     inglob = "{}/*.{}".format(inpath, fmt1)
     for fin in glob.glob(inglob):
         inname = os.path.basename(fin).split('.')[0]
-        fout = "{}/{}".format(outpath,inname)
+        fout = "{}/{}".format(outpath, inname)
         meta = meta_fn(fin)
         cmd = shell.format(fin=fin, **meta, fout=fout)
         print(cmd)
@@ -253,14 +268,14 @@ def hpmenc(inpath, outpath, qplist=[]):
         inname = os.path.basename(fin).split('.')[0]
         meta = meta_fn(fin)
         for qp in qplist:
-            fout = "{outpath}/{inname}_{qp}".format(outpath=outpath,inname=inname,qp=qp)
-            cmd = shell.format(base=base,fin=fin, **meta, fout=fout, qp=qp)
+            fout = "{}/{}_{:02d}".format(outpath, inname, qp)
+            cmd = shell.format(base=base, fin=fin, **meta, fout=fout, qp=qp)
             #print(cmd)
             TASKS.append(cmd)
 
 
 def vtmenc(inpath, outpath, qplist=[56]):
-    base = Conf.host["hpm.base"]
+    base = Conf.host["vtm.base"]
     shell = ' '.join([
         "{base}/bin/EncoderAppStatic",
         "-c {base}/cfg/encoder_intra_vtm.cfg",
@@ -273,8 +288,28 @@ def vtmenc(inpath, outpath, qplist=[56]):
         inname = os.path.basename(fin).split('.')[0]
         meta = meta_fn(fin)
         for qp in qplist:
-            fout = "{outpath}/{inname}_{qp}".format(outpath=outpath,inname=inname,qp=qp)
-            cmd = shell.format(base=base,fin=fin, **meta, fout=fout, qp=qp)
+            fout = "{}/{}_{:02d}".format(outpath, inname, qp)
+            cmd = shell.format(base=base, fin=fin, **meta, fout=fout, qp=qp)
+            TASKS.append(cmd)
+
+
+def vtmencrgb(inpath, outpath, qplist=[56]):
+    base = Conf.host["vtm.base"]
+    shell = ' '.join([
+        "{base}/bin/EncoderAppStatic",
+        "-c {base}/cfg/encoder_intra_vtm.cfg",
+        "-i {fin} -wdt {SourceWidth} -hgt {SourceHeight} -fr 30 -f 1 -q {qp} --InputChromaFormat=444",
+        "--InputBitDepth={InputBitDepth} --OutputBitDepth={InputBitDepth} --ConformanceMode",
+        "--InputColourSpaceConvert=RGBtoGBR --SNRInternalColourSpace=1 --OutputInternalColourSpace=0",  # improve ~0.05dB
+        "-b {fout}.bin -o {fout}.rgb > {fout}.log"
+    ])
+    inglob = "{inpath}/*.rgb".format(inpath=inpath)
+    for fin in glob.glob(inglob):
+        inname = os.path.basename(fin).split('.')[0]
+        meta = meta_fn(fin)
+        for qp in qplist:
+            fout = "{}/{}_{:02d}".format(outpath, inname, qp)
+            cmd = shell.format(base=base, fin=fin, **meta, fout=fout, qp=qp)
             TASKS.append(cmd)
 
 
@@ -287,7 +322,7 @@ def hpmcrop(inpath, outpath):
     inglob = "{inpath}/*.yuv".format(inpath=inpath)
     for fin in glob.glob(inglob):
         inname = os.path.basename(fin).split('.')[0]
-        fout = "{outpath}/{inname}".format(outpath=outpath,inname=inname)
+        fout = "{outpath}/{inname}".format(outpath=outpath, inname=inname)
         meta = meta_fn(fin)
         meta["TrueWidth"] = -(int(meta['SourceWidth'])//-8)*8
         meta["TrueHeight"] = -(int(meta['SourceHeight'])//-8)*8
@@ -298,7 +333,7 @@ def hpmcrop(inpath, outpath):
 def show(inpath, outpath):
     print('--- Analyze encode logs. ---')
     tasks = sorted(glob.glob(inpath+"/*.log"))
-    count = {"wait": 0, "excute":0, "finish": 0}
+    count = {"wait": 0, "excute": 0, "finish": 0}
     enctype = log_getEnctype(tasks[0])
     results = []
     for fn in tasks:
@@ -310,8 +345,8 @@ def show(inpath, outpath):
         if status != "finish":
             print("[{}] {}.log".format(status, inname))
     print('Total %d tasks, %d wait, %d excute, %d finish.' %
-            (len(tasks), count["wait"], count["excute"], count["finish"]))
-    with open("enclog.csv","w") as f:
+          (len(tasks), count["wait"], count["excute"], count["finish"]))
+    with open("enclog.csv", "w") as f:
         f.write('fn,'+','.join(LOG_KEYS[enctype])+"\n")
         for result in results:
             f.write(','.join(result)+'\n')
@@ -321,28 +356,28 @@ def show(inpath, outpath):
 def netop(inpath, outpath, op):
     cmds = [
         "rm /home/medialab/faymek/iir/datasets/cli/*",
-        "mv %s/* /home/medialab/faymek/iir/datasets/cli/"%inpath,
-        "/home/medialab/miniconda3/envs/iir/bin/python /home/medialab/faymek/iir/codes/test_%s.py -opt ~/faymek/iir/codes/options/test/%s.yml"%(op[-1], op),
-        "mv /home/medialab/faymek/iir/results/test/%s/* %s/"%(op, outpath)
+        "mv %s/* /home/medialab/faymek/iir/datasets/cli/" % inpath,
+        "/home/medialab/miniconda3/envs/iir/bin/python /home/medialab/faymek/iir/codes/test_%s.py -opt ~/faymek/iir/codes/options/test/%s.yml" % (
+            op[-1], op),
+        "mv /home/medialab/faymek/iir/results/test/%s/* %s/" % (op, outpath)
     ]
-    TASKS.extend(cmds) 
+    TASKS.extend(cmds)
 
 
 def call_script(script):
     desc = script.split('/')[-1]
     stamp = time.strftime("%m-%d %H:%M", time.localtime())
-    print("- [%s] start :"%stamp, desc)
-    #time.sleep(5)
+    print("- [%s] start :" % stamp, desc)
     re = subprocess.run(script, shell=True, capture_output=True)
     # print(re.stderr)
     stamp = time.strftime("%m-%d %H:%M", time.localtime())
-    print("- [%s] finish:"%stamp, desc)
+    print("- [%s] finish:" % stamp, desc)
 
 
 if __name__ == '__main__':
-    funcMap = {'yuv1stframe':yuv1stframe, 'netop':netop, 
-                 'hpmenc': hpmenc, 'hpmcrop': hpmcrop,
-                 'vtmenc':vtmenc, 'show':show}
+    funcMap = {'yuv1stframe': yuv1stframe, 'netop': netop,
+               'hpmenc': hpmenc, 'hpmcrop': hpmcrop,
+               'vtmenc': vtmenc, 'vtmencrgb': vtmencrgb, 'show': show}
 
     parser = argparse.ArgumentParser(
         description='Media Encoding Batch Utils. Copyright @ 2016-2020')
@@ -361,7 +396,7 @@ if __name__ == '__main__':
                         help="network operation")
     parser.add_argument("--cf", type=str, default="420", choices=["420", "422", "444"],
                         help="chroma format")
-    
+
     args = parser.parse_args()
     args.inpath = getabspath(args.inpath)
     args.outpath = getabspath(args.outpath)
@@ -382,18 +417,18 @@ if __name__ == '__main__':
     elif args.verb in ['show']:
         funcMap[args.verb](args.inpath, args.outpath)
         sys.exit()
-    elif args.verb in ['hpmenc', 'vtmenc']:
+    elif args.verb in ['hpmenc', 'vtmenc', 'vtmencrgb']:
         funcMap[args.verb](args.inpath, args.outpath, args.qps)
     elif args.verb == 'netop':
         netop(args.inpath, args.outpath, args.op)
     else:
         funcMap[args.verb](args.inpath, args.outpath)
-    with open("tasks.json","w") as f:
+    with open("tasks.json", "w") as f:
         json.dump(TASKS, f, indent=4)
-    
-    if args.host == "off": # run instant, without server
-        print('Excute the %d shell script with %d process.\n' % \
-            (len(TASKS), args.core))
+
+    if args.host == "off":  # run instant, without server
+        print('Excute the %d shell script with %d process.\n' %
+              (len(TASKS), args.core))
         RunPool = Pool(args.core)
         RunPool.map_async(call_script, TASKS)
         RunPool.close()
@@ -402,42 +437,41 @@ if __name__ == '__main__':
     else:  # server mode, check server
         Conf.baseurl = 'http://{}:42024'.format(Conf.host["ip"])
         try:
-            print("Host %s : "%args.host+get("/id"))
+            print("Host %s : " % args.host+get("/id"))
         except:
-            print("Host %s : Server Not Running!"%args.host)
+            print("Host %s : Server Not Running!" % args.host)
             sys.exit()
 
         remdir = get("/path")
         key = remdir.split('/')[-1]
-        print("Job key: %s"%key)
+        print("Job key: %s" % key)
         remin = remdir + "/inpath"
         remout = remdir + "/outpath"
-        
+
         if args.host != "local":
             for i in range(len(TASKS)):
-                TASKS[i] = TASKS[i].replace(args.inpath, remin).replace(args.outpath, remout)
-            with open("tasks.json","w") as f:
+                TASKS[i] = TASKS[i].replace(
+                    args.inpath, remin).replace(args.outpath, remout)
+            with open("tasks.json", "w") as f:
                 json.dump(TASKS, f, indent=4)
             print("\n--- SCP uploading ---\n")
-            os.system("scp tasks.json {}:{}/".format(args.host,remdir))
-            os.system("scp -r {}/* {}:{}/".format(args.inpath,args.host,remin))
+            os.system("scp tasks.json {}:{}/".format(args.host, remdir))
+            os.system("scp -r {}/* {}:{}/".format(args.inpath, args.host, remin))
         else:
             os.system("cp tasks.json {}/".format(remdir))
-        
-        pf = {'fpath':remdir+"/tasks.json",'core':args.core, 'key':key}
+
+        pf = {'fpath': remdir+"/tasks.json", 'core': args.core, 'key': key}
         print(post("/add", pf))
         while True:
             left = get("/busy?"+key)
             stamp = time.strftime("%m-%d %H:%M", time.localtime())
-            print("- [%s]: %s jobs left"%(stamp, left))
+            print("- [%s]: %s jobs left" % (stamp, left))
             if left == "0":
                 break
             time.sleep(args.wait)
-        
+
         if args.host != "local":
             print("\n--- SCP downloading ---\n")
-            os.system("scp -r {}:{}/* {}".format(args.host,remout,args.outpath))
+            os.system("scp -r {}:{}/* {}".format(args.host, remout, args.outpath))
 
     print("--- Job done. ---")
-
-
